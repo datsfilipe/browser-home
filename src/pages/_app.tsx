164125d97d5a42bef608ad71d4ddd344bef301f4
datsfilipe <datsfilipe@gmail.com>
2021-09-11 15:44:54 -0300

    remove semicolon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components'
 
-import theme from '../styles/Themes/theme';
-import GlobalStyle from '../styles/global';
+import theme from '../styles/Themes/theme'
+import GlobalStyle from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
