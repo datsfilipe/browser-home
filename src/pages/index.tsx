55052ef6bd1094578c65fb1cb527fd9fd1931c01
datsfilipe <datsfilipe@gmail.com>
2021-09-11 15:48:47 -0300

    add auth functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,77 @@ import type { NextPage } from 'next'
 import Head from 'next/head';
 import Image from 'next/image';
 
+import accountImg from '../assets/account.svg';
 import supportImg from '../assets/support-img.svg';
 
 import {Container, Main, Categories, Aside, Footer, AuthButton} from '../styles/home';
 import { Menu } from '../components/Menu/index';
 import { Form } from '../components/Form/index';
+import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth';
+import { auth } from '../services/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 const Home: NextPage = () => {
+
+  const defaultUserTemplate = {
+    id: '',
+    name: 'Faça Login',
+    avatar: accountImg
+  }
+
+  const [user, setUser] = useState<User>(defaultUserTemplate);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google account');
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    })
+
+    return () => {
+      unsubscribe();
+    }
+  }, [])
+
+  async function signIn () {
+    if (user === defaultUserTemplate) {
+      const provider = new GoogleAuthProvider();
+
+      const result = await signInWithPopup(auth, provider);
+      if (result.user) {
+        const { displayName, photoURL, uid } = result.user;
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google account')
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    }
+  }
+
+  console.log(user)
 
   return (
     <Container>
@@ -26,7 +90,7 @@ const Home: NextPage = () => {
           {user.name}
         </AuthButton>
         <Aside>
-          <Image src={supportImg} alt="Imagem de Banner" />
+          <Image src={supportImg} alt="Banner" />
         </Aside>
 
         <div id="container">
@@ -42,6 +106,7 @@ const Home: NextPage = () => {
       {/* <Footer>
         <a href="">Banner</a>/ with license: <a href="https://creativecommons.org/licenses/by/4.0/">Attribution 4.0 International (CC BY 4.0)</a>
         <a href="https://iconscout.com/icons/plus" target="_blank">Plus Icon</a> on <a href="https://iconscout.com">Iconscout</a>
+        <a href="https://iconscout.com/icons/account" target="_blank">Account Icon</a> on <a href="https://iconscout.com">Iconscout</a>
       </Footer> */}
     </Container>
   )
