84e462bd280841521ef3a86fd8e773f9b8c821e8
datsfilipe <datsfilipe@gmail.com>
2021-10-13 13:39:26 -0300

    separate types

---
+++ src/types/menus.d.ts
@@ -0,0 +1,24 @@
+export type Menus = {
+  id: string;
+  first_menu: {
+    title: string;
+    items?: {
+      name: string;
+      url: string;
+    }
+  }
+  second_menu: {
+    title: string;
+    items?: {
+      name: string;
+      url: string;
+    }
+  }
+  third_menu: {
+    title: string;
+    items?: {
+      name: string;
+      url: string;
+    }
+  }
+}
