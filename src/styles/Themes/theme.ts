6af85427454ba477348840a693ab102934169a37
datsfilipe <datsfilipe@gmail.com>
2021-10-17 02:37:15 -0300

    update theme

--- src/styles/Themes/theme.ts
+++ src/styles/Themes/theme.ts
@@ -1,12 +1,12 @@
 const theme = {
-  dark: {
-    background: '#191622',
-    text: '#ffff80',
-    input_text: '#ff9580',
-    primary: '#9580ff',
-    outline_effect: '#8aff80',
-    border: '#ff80bf'
-  }
+  background: '#191622',
+  primary: '#252131',
+  border: '#44475A75',
+  effects: '#44475A75',
+  text: '#ffff80',
+  textSecondary: '#fefefe',
+  textTertiary: '#ff9580',
+  purple: '#9580ff'
 }
 
 export default theme
